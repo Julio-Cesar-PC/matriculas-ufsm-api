6aa18c0b358893d9cbb569efacb993ca0cec8ed5
Julio Cesar Polmann <84574865+Julio-Cesar-PC@users.noreply.github.com>
2024-11-28 11:23:08 -0300

    fix solicitação de matricula

--- src/controllers/turma.js
+++ src/controllers/turma.js
@@ -285,8 +285,6 @@ class TurmaController {
             if (turmasDisponiveis.length === 0) {
                 return res.status(404).send('Nenhuma turma disponível para o aluno!');
             }
-
-            console.log(turmasDisponiveis);
     
             // Transformando o campo `horarios` para um array de objetos
             turmasDisponiveis.forEach(turma => {
